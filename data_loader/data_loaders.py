da0d7f428429e85bc6758196f2a2df38fdbab9eb
TaeHong <josephmoon2019@gmail.com>
2020-05-21 14:47:42 +0900

    Simplifying the dataloader object

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,33 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
 
-
 class MnistDataLoader(BaseDataLoader):
     """
-    MNIST data loading demo using BaseDataLoader
+    MNIST data loading using BaseDataLoader
     """
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        rescaling = lambda x: (x - .5) * 2.
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            rescaling
         ])
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class CIFAR10DataLoader(BaseDataLoader):
+    """
+    CIFAR10 data loading using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        rescaling = lambda x: (x - .5) * 2.
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            rescaling
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=True, transforms=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+# TODO test data_loader should have shuffled two different datasets.
