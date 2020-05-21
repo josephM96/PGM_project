e34f57ca6f0db549c3c7acb195de59c2bc099589
TaeHong <josephmoon2019@gmail.com>
2020-05-21 19:25:56 +0900

    Must review this test-code once again

--- test.py
+++ test.py
@@ -14,57 +14,54 @@ def main(config):
     # setup data_loader instances
     data_loader = getattr(module_data, config['data_loader']['type'])(
         config['data_loader']['args']['data_dir'],
-        batch_size=512,
+        batch_size=64,
         shuffle=False,
         validation_split=0.0,
         training=False,
-        num_workers=2
+        num_workers=1
     )
 
     # build model architecture
-    model = config.init_obj('arch', module_arch)
-    logger.info(model)
+    model_background = config.init_obj('arch_background', module_arch)
+    model_semantic = config.init_obj('arch_semantic', module_arch)
+
+    logger.info(model_background)
+    logger.info(model_semantic)
 
     # get function handles of loss and metrics
     loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
     checkpoint = torch.load(config.resume)
     state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
-        model = torch.nn.DataParallel(model)
-    model.load_state_dict(state_dict)
+        model = torch.nn.DataParallel(model_background)
+    model_background.load_state_dict(state_dict)
+    model_semantic.load_state_dict(state_dict)
 
     # prepare model for testing
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = model.to(device)
-    model.eval()
+    model_b = model_background.to(device)
+    model_s = model_semantic.to(device)
+    model_b.eval()
+    model_s.eval()
 
     total_loss = 0.0
-    total_metrics = torch.zeros(len(metric_fns))
 
     with torch.no_grad():
         for i, (data, target) in enumerate(tqdm(data_loader)):
             data, target = data.to(device), target.to(device)
-            output = model(data)
-
-            #
-            # save sample images, or do something with output here
-            #
+            output_b, output_s = model_b(data), model_s(data)
 
             # computing loss, metrics on test set
-            loss = loss_fn(output, target)
+            loss_b = loss_fn(output_b, target)
+            loss_s = loss_fn(output_s, target)
+
             batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
+            total_loss += (loss_s.item() - loss_b.item()) * batch_size
 
     n_samples = len(data_loader.sampler)
     log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
     logger.info(log)
 
 
